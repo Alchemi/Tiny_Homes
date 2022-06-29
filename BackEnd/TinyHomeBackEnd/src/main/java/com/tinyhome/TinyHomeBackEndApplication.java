14ede8e72edce3ce212bba1878842af48bb05359
Gandarilla <alejandrogandarilla14@gmail.com>
2022-06-29 12:42:48 -0700

    Backend

---
+++ BackEnd/TinyHomeBackEnd/src/main/java/com/tinyhome/TinyHomeBackEndApplication.java
@@ -0,0 +1,13 @@
+package com.tinyhome;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TinyHomeBackEndApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TinyHomeBackEndApplication.class, args);
+	}
+
+}
---
+++ BackEnd/TinyHomeBackEnd/src/test/java/com/tinyhome/TinyHomeBackEndApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tinyhome;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TinyHomeBackEndApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
