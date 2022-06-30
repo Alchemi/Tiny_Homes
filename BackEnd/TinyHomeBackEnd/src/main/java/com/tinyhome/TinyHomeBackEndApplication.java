c4a0e9c1949d80fe3ebfce11df0a4fbc5c0d6742
sosotune <tslxg2019@gmail.com>
2022-06-30 11:56:01 -0400

    made some changes to house model

--- BackEnd/TinyHomeBackEnd/src/main/java/com/tinyhome/TinyHomeBackEndApplication.java
+++ BackEnd/TinyHomeBackEnd/src/main/java/com/tinyhome/TinyHomeBackEndApplication.java
@@ -1,13 +1,15 @@
+
 package com.tinyhome;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
-public class TinyHomeBackEndApplication {
+	@SpringBootApplication
+	public class TinyHomeBackEndApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(TinyHomeBackEndApplication.class, args);
+		public static void main(String[] args) {
+			SpringApplication.run(TinyHomeBackEndApplication.class, args);
+		}
+
 	}
 
-}
